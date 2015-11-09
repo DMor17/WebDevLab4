c1b98c7a3f115a5784bd4a3e85abdf5b0a397fe5
Daniel <dannymorison17@Gmail.com>
2015-11-09 15:03:17 +0000

    Added code for index.

--- index.php
+++ index.php
@@ -1,7 +1,48 @@
+
+
+
+<html>
+<head></head>
+<body>
+<p>Wikipedia test</p>
+
+<form action="index.php" method="POST">
+    <select name="selectID" id="selectID" onchange="this.form.submit()">
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+        <option value="4">Four</option>
+    </select>
+</form>
+</body>
+
+</html>
+
+
 <?php
-/**
- * Created by PhpStorm.
- * User: daniel
- * Date: 09/11/15
- * Time: 13:49
- */
+$sectionID = 0;
+$db = new mysqli(
+    "br-cdbr-azure-south-a.cloudapp.net",
+    "bd7768bee1ccf1",
+    "b0434b31",
+    "dm1406997"
+);
+
+
+$sql_query = "Select * From sectionText WHERE sectionID Like $_POST(selectID)";
+$qr = $db->query($sql_query);
+while ($row = $qr->fetch_array()) {
+    echo "<p>" . $row . "</p>";
+
+}
+$qr->close();
+$db->close();
+// test if connection was established, and print any errors
+if($db->connect_errno) {
+
+    die('Connectfailed[' . $db->connect_error . ']');
+
+
+
+
+}
